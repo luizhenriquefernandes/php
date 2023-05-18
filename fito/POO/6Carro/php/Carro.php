df30725ff70527a83ca68eb34bcb10267a4282fa
luizhenriquefernandes <luizhenrique.bio@gmail.com>
2023-05-17 21:18:33 -0300

    Classe Carro

--- fito/POO/6Carro/php/Carro.php
+++ fito/POO/6Carro/php/Carro.php
@@ -1,26 +1,50 @@
 <?php
     class Carro{
-        private $name;       
-
-        /**
-         * Get the value of name
-         */
+        private $name; 
+        private $key = false;
+        public function getKey(){
+            return $this->key;
+        }  
+        public function setKey($key){
+            $this->key = $key;
+        }         
         public function getName()
         {
                 return $this->name;
-        }
-
-        /**
-         * Set the value of name
-         */
+        }       
         public function setName($name): self
         {
                 $this->name = $name;
-
                 return $this;
         }
+        public function inserirChave(){
+            $this->setKey($this->getKey()+true);     
+
+        }
+        public function retirarChave(){
+            $this->setKey(false);
+           
+        }
+        
         public function ligarCarro(){
+            if($this->getKey()==true){
             print('<br>Estou ligado bruuummmm<br>');
+        }
+            else{
+                print('<br>Estou desligado porque não inseriu a chave');
+            }
+    }
+        public function desligarCarro(){
+            if($this->getKey()==true){
+                
+                print('<br>Não posso desligar retire a chave<br>');
+            }
+            else{
+                print('<br>Já desliguei<br>');
+            }
+        }
+        public function status(){
+            print("O carro é {$this->getName()}");
         }
     }
 ?>
--- fito/POO/6Carro/php/crudCarro.php
+++ fito/POO/6Carro/php/crudCarro.php
@@ -1,7 +1,30 @@
-<?php
-    require_once "./Carro.php";    
-    $carroAtual = new Carro();
-    $carroAtual->setName($_GET['txtNome']);    
-    print("O carro é {$carroAtual->getName()}");
-    $carroAtual->ligarCarro();
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>PDO -> CARRO</title>
+</head>
+<body>
+    <div class="container">
+        <div class="formularioB">
+            <?php
+                require_once "./Carro.php";    
+                $carroAtual = new Carro();
+                $carroAtual->setName($_GET['txtNome']);    
+                // print("O carro é {$carroAtual->getName()}");                             
+                $carroAtual->status();
+                $carroAtual->inserirChave();
+                $carroAtual->ligarCarro();
+                $carroAtual->retirarChave();
+                $carroAtual->desligarCarro();
+
+            ?>
+        </div>
+    </div>
+</body>
+</html>
+
+
